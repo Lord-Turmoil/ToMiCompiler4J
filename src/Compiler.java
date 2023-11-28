15d51294a230468fafb4b9c3953390a87eda4208
Lord Turmoil <tony-turmoil@outlook.com>
2023-11-28 09:34:07 +0800

    update: main config

--- src/Compiler.java
+++ src/Compiler.java
@@ -8,8 +8,6 @@ import tomic.core.Config;
 import tomic.core.ToMiCompiler;
 
 public class Compiler {
-    private static boolean showHelp = false;
-
     public static void main(String[] args) {
         Config config = new Config();
 
